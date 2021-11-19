c4c2b7ea21feb69bcc2c67c97e4694c55a8e217a
fatihCinarKrtg <63067360+fatihCinarKrtg@users.noreply.github.com>
2021-11-19 16:43:15 +0300

    auth revisions

--- static/js/popover_menus.js
+++ static/js/popover_menus.js
@@ -43,8 +43,7 @@ export function initialize() {
             instance.setContent(
                 render_left_sidebar_stream_setting_popover({
                     can_create_streams:
-                        settings_data.user_can_create_private_streams() ||
-                        settings_data.user_can_create_public_streams(),
+                        settings_data.user_can_unsubscribe_other_users()
                 }),
             );
             left_sidebar_stream_setting_popover_displayed = false;
